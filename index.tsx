c7f2782a16d1757bc343c23c4109c42b82d66657
Linlin Xie <xielinlin2998@163.com>
2023-07-04 18:24:00 -0400

    adjust component structure

---
+++ index.tsx
@@ -0,0 +1,47 @@
+import { GetServerSideProps } from "next";
+
+import { HomeProps } from "./pages/Interface/interfaces";
+import ImageFeed from "./pages/components/ImageFeedPage";
+
+const Home: React.FC<HomeProps> = ({ characters, page, totalPages, error }) => {
+  if (error) {
+    return <h1>{error}..., Refresh the page to retry!</h1>; // Display error message page
+  }
+  return (
+    <div>
+      <ImageFeed characters={characters} page={page} totalPages={totalPages} />
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async (
+  context
+) => {
+  try {
+    const page = Number(context.query.page) || 1;
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}`
+    );
+    const data = await response.json();
+
+    return {
+      props: {
+        characters: data.results,
+        page,
+        totalPages: data.info.pages,
+        error: "",
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        characters: [],
+        page: 0,
+        totalPages: 0,
+        error: "Something went wrong",
+      },
+    };
+  }
+};
+
+export default Home;
