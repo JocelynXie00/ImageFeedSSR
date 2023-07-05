225d6e65268d05e56f2b6c0a5ea09e3f9e025005
Linlin Xie <xielinlin2998@163.com>
2023-07-05 14:02:37 -0400

    update code structure

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "41ffbd22484492cf"; }
+/******/ 		__webpack_require__.h = function() { return "aa2fd408ace7caea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,13 @@
-import { GetServerSideProps } from "next";
-
 import { HomeProps } from "../Interface/interfaces";
-import ImageFeed from "./components/ImageFeedPage";
+import Home, { getServerSideProps } from "./components/Home";
 
-const Home: React.FC<HomeProps> = ({ characters, page, totalPages, error }) => {
-  if (error) {
-    return <h1>{error}..., Refresh the page to retry!</h1>; // Display error message page
-  }
+const IndexPage: React.FC<HomeProps> = (props) => {
   return (
     <div>
-      <ImageFeed characters={characters} page={page} totalPages={totalPages} />
+      <Home {...props} />
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async (
-  context
-) => {
-  try {
-    const page = Number(context.query.page) || 1;
-    const response = await fetch(
-      `https://rickandmortyapi.com/api/character?page=${page}`
-    );
-    const data = await response.json();
-
-    return {
-      props: {
-        characters: data.results,
-        page,
-        totalPages: data.info.pages,
-        error: "",
-      },
-    };
-  } catch (error) {
-    return {
-      props: {
-        characters: [],
-        page: 0,
-        totalPages: 0,
-        error: "Something went wrong",
-      },
-    };
-  }
-};
-
-export default Home;
+export { getServerSideProps };
+export default IndexPage;
